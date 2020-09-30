e45b7eb2caa0d9cd63342687eec252e756f99060
Vlad Zobnin <vladopodobniy@gmail.com>
2020-09-30 23:07:34 +0300

    add colors

--- script.js
+++ script.js
@@ -8,23 +8,56 @@ const crosshair = document.body.onmousemove = (event) => {
     document.getElementById('div').style = `left: ${x - 100}px; top: ${y}px;`;
 };
 
-document.body.onmousedown = () => {
-    document.body.onmousemove = (event) => {
 
-        let excretion = document.createElement('div');
-        excretion.id = ('excretion')
-        excretion.style = `
-        background: rgba(97, 166, 231, 0.247);
-        border: solid rgb(63, 65, 196);
-        position: absolute;
-        width: ${50}px;
-        height: ${50 }px;
-        left: ${ event.pageX }px;
-        top: ${ event.pageY }px;
-         `;
-        document.body.appendChild(excretion);
+function paint (event) {
+
+    let green = document.querySelector('#green');
+    let black = document.querySelector('#black');
+    let yellow = document.querySelector('#yellow');
+    let red = document.querySelector('#red');
+
+    function moveColor (event) {      
+    let a = 'rgba(97, 166, 231, 0.247)'; 
+    let excretion = document.createElement('div');
+    excretion.id = ('excretion')
+
+    green.addEventListener('click', () => {
+        excretion.style.backgroundColor = 'rgba(71, 225, 78, 0.247)';
+        excretion.style.boxShadow = 'rgba(71, 225, 78, 0.247) 0 0 20px 10px';
+    })
+    black.addEventListener('click', () => {
+        excretion.style.backgroundColor = 'rgba(30,30,30, 0.247)';
+        excretion.style.boxShadow = 'rgba(30,30,30, 0.247) 0 0 20px 10px';
+    })
+    yellow.addEventListener('click', () => {
+        excretion.style.backgroundColor = 'rgba(255,219,0, 0.247)';
+        excretion.style.boxShadow = 'rgba(255,219,0, 0.247) 0 0 20px 10px';
+    })
+    red.addEventListener('click', () => {
+        excretion.style.backgroundColor = 'rgba(255,8,0, 0.247)';
+        excretion.style.boxShadow = 'rgba(255,8,0, 0.247) 0 0 20px 10px';
+    })
+
+    excretion.style = `
+    background-color: ${a};
+    box-shadow:  rgba(97, 166, 231, 0.247) 0 0 20px 10px; 
+    position: absolute;
+    border-radius: 50px;
+    width: ${50}px;
+    height: ${50 }px;
+    left: ${ event.pageX }px;
+    top: ${ event.pageY }px;
+     `;
+
+    document.body.appendChild(excretion);
     }
+
+    document.addEventListener('mousemove', moveColor);
+
+    document.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', moveColor);
+    })
 }
-document.body.onmouseup = () => {
-    document.getElementById("excretion").remove();
-}
+
+
+document.addEventListener('mousedown', paint);
