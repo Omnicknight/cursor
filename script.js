4f67a919831f6bce3e190fb62f3f360374b1d912
Wengo500 <wengo500@gmail.com>
2020-09-30 22:38:52 +0300

    commit

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
-const crosshair = document.body.onmousemove = (event) => {
-    let x = event.pageX;
-    let y = event.pageY
-
-    const xy = document.querySelector("#xy");
-    xy.innerHTML = `${x} / ${y}`
-
-    document.getElementById('div').style = `left: ${x - 100}px; top: ${y}px;`;
-};
-
 document.body.onmousedown = () => {
     document.body.onmousemove = (event) => {
 
         let excretion = document.createElement('div');
         excretion.id = ('excretion')
         excretion.style = `
-        background: rgba(97, 166, 231, 0.247);
-        border: solid rgb(63, 65, 196);
+        background: rgba(12, 184, 40, 0.028);
+       box-shadow: rgba(12, 184, 40, 0.038) 0 0 10px 20px;
+       border-radius: 50px;
         position: absolute;
-        width: ${50}px;
-        height: ${50 }px;
+        width: ${30}px;
+        height: ${30 }px;
         left: ${ event.pageX }px;
         top: ${ event.pageY }px;
          `;
         document.body.appendChild(excretion);
     }
-}
-document.body.onmouseup = () => {
-    document.getElementById("excretion").remove();
 }
